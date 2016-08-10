a02681fc84aabbb72358890f54b0f92bde4d43b4
Alvaro Muhlethaler <amuhlethaler@vairix.com>
2016-08-10 00:46:40 -0300

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+
+  render: function() {
+    return (
+      <div>This is the Countdown</div>
+      );
+  }
+
+});
+
+module.exports = Countdown;
